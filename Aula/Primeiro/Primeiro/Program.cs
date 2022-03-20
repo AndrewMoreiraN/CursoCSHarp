56c1b4da53e04dfea6a0a3b215a4ec8f28e4d173
AndrewMoreiraN <andrewmoreira02@hotmail.com>
2022-03-20 17:57:14 -0300

    Deletando o txt.

---
+++ Aula/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primeiro
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
