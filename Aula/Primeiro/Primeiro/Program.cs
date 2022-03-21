218e248d2d1b822f56b8f6081d39075d900766e3
AndrewMoreiraN <andrewmoreira02@hotmail.com>
2022-03-21 19:10:31 -0300

    Exercicio 1

--- Aula/Primeiro/Primeiro/Program.cs
+++ Aula/Primeiro/Primeiro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace PrimeiroProjeto
 {
@@ -7,24 +8,48 @@ namespace PrimeiroProjeto
         private static void Main(string[] args)
         {
             bool complete = false;
+            byte n1 = 126;
             char genero = 'F';
             char letra = '\u0041';
-            byte n1 = 126;
+            decimal n10 = decimal.MaxValue;
+            decimal n9 = decimal.MinValue;
+            double n6 = 4.515123;
+            float n5 = 4.518312f;
             int n2 = 1000;
             int n3 = 2147483647;
-            long n4 = 2147483648L;
-            float n5 = 4.5f;
-            double n6 = 4.5;
-            Object obj2 = 4.5;
-            string nome = "Andrew";
-            Object obj1 = "Andrew Moreira";
-
             int n7 = int.MinValue;
             int n8 = int.MaxValue;
-            decimal n9 = decimal.MinValue;
-            decimal n10 = decimal.MaxValue;
-            Console.WriteLine(n9);
-            Console.WriteLine(n10);
+            long n4 = 2147483648L;
+            Object obj1 = "Andrew Moreira";
+            Object obj2 = 4.5;
+            string nome = "Andrew";
+            //definir numero de casas decimais
+            //Console.WriteLine(n6.ToString("F3"));
+            //colocar . ao invés de ,
+            //Console.WriteLine(n5.ToString("F2", CultureInfo.InvariantCulture));
+            //Console.WriteLine(n9);
+            //Console.WriteLine(n10);
+            //Console.WriteLine(ExercioDeFixacao());
+        }
+
+        private static String ExercioDeFixacao()
+        {
+            string resultado = "";
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            resultado = $"Produtos:\n{produto1}, cujo preço é $ {preco1.ToString("F2")}\n{produto2}, cujo preço é ${preco2.ToString("F2")}\n\nRegistro: {idade} anos de idade, código {codigo} e gênero: {genero}\n\nMedida com oito casas decimais: {medida.ToString("F8")}\nArredondado (três casas decimais): {medida.ToString("F3")}\nSeparador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}";
+
+            return resultado;
         }
     }
 }
---
+++ Exercicios/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Exercício 1:");
+            Console.WriteLine(Exercicio1());
+            Console.WriteLine(Exercicio1());
+            Console.WriteLine(Exercicio1());
+
+            Console.WriteLine($"\nExercício 2:");
+            Console.WriteLine(Exercicio2());
+            Console.WriteLine(Exercicio2());
+            Console.WriteLine(Exercicio2());
+
+            Console.WriteLine($"\nExercício 3:");
+            Console.WriteLine(Exercicio3());
+            Console.WriteLine(Exercicio3());
+
+            Console.WriteLine($"\nExercício 4:");
+            Console.WriteLine(Exercicio4());
+            Console.WriteLine(Exercicio4());
+            Console.WriteLine(Exercicio4());
+
+            Console.WriteLine($"\nExercício 5:");
+            Console.WriteLine(Exercicio5());
+            Console.WriteLine(Exercicio5());
+            Console.WriteLine(Exercicio5());
+
+            Console.WriteLine($"\nExercício 6:");
+            Console.WriteLine(Exercicio6());
+            Console.WriteLine(Exercicio6());
+        }
+
+        private static String Exercicio1()
+        {
+            int numero1 = int.Parse(Console.ReadLine());
+            int numero2 = int.Parse(Console.ReadLine());
+
+            return $"\tSOMA = {numero1 + numero2}\n";
+        }
+
+        private static String Exercicio2()
+        {
+            double raio = double.Parse(Console.ReadLine());
+
+            return $"\tA = {(3.14159 * Math.Pow(raio, 2)).ToString("F4", CultureInfo.InvariantCulture)}\n";
+        }
+
+        private static String Exercicio3()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            return $"\tDIFERENÇA = {a * b - c * d}\n";
+        }
+
+        private static String Exercicio4()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int hoursWorked = int.Parse(Console.ReadLine());
+            float salaryPerHour = float.Parse(Console.ReadLine());
+
+            return $"\tNUMBER = {number}\n\tSALARY = U$ {(hoursWorked * salaryPerHour).ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        private static String Exercicio5()
+        {
+            string[] string1 = Console.ReadLine().Split(' ');
+            string[] string2 = Console.ReadLine().Split(' ');
+            int code1 = int.Parse(string1[0]);
+            int code2 = int.Parse(string2[0]);
+            int number1 = int.Parse(string1[1]);
+            int number2 = int.Parse(string2[1]);
+            float price1 = float.Parse(string1[2]);
+            float price2 = float.Parse(string2[2]);
+
+            return $"\tVALOR A PAGAR: R$ {(number1 * price1 + number2 * price2).ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        private static String Exercicio6()
+        {
+            string[] string1 = Console.ReadLine().Split(' ');
+            float a = float.Parse(string1[0]);
+            float b = float.Parse(string1[1]);
+            float c = float.Parse(string1[2]);
+
+            return $"\tTRIANGULO: {(a * c / 2).ToString("F3", CultureInfo.InvariantCulture)}\n" +
+                    $"\tCIRCULO: {(Math.Pow(c, 2) * 3.14159).ToString("F3", CultureInfo.InvariantCulture)}\n" +
+                    $"\tTRAPEZIO: {((a + b) / 2 * c).ToString("F3", CultureInfo.InvariantCulture)}\n" +
+                    $"\tQUADRADO: {(Math.Pow(b, 2)).ToString("F3", CultureInfo.InvariantCulture)}\n" +
+                    $"\tRETANGULO: {(a * b).ToString("F3", CultureInfo.InvariantCulture)}\n";
+        }
+    }
+}
