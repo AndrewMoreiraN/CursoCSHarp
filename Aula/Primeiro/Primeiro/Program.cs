1f7b2084425665b23f1464e3a4362ca9c697fc2f
AndrewMoreiraN <andrewmoreira02@hotmail.com>
2022-03-21 17:05:57 -0300

    Primeiro projeto

--- Aula/Primeiro/Primeiro/Program.cs
+++ Aula/Primeiro/Primeiro/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace Primeiro
+namespace PrimeiroProjeto
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool complete = false;
+            char genero = 'F';
+            char letra = '\u0041';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            Object obj2 = 4.5;
+            string nome = "Andrew";
+            Object obj1 = "Andrew Moreira";
+
+            int n7 = int.MinValue;
+            int n8 = int.MaxValue;
+            decimal n9 = decimal.MinValue;
+            decimal n10 = decimal.MaxValue;
+            Console.WriteLine(n9);
+            Console.WriteLine(n10);
         }
     }
 }
