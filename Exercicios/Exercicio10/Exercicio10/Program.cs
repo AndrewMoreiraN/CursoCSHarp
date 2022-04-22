cbad64eff46a795404f3aadf043e56ed7c2a9948
AndrewMoreiraN <andrewmoreira02@hotmail.com>
2022-04-22 18:35:34 -0300

    Matrizes

---
+++ Exercicios/Exercicio10/Exercicio10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Exercicio10
+{
+    internal class Program
+    {
+        private static int Transforma_Para_Int(string numero)
+        {
+            return int.Parse(numero);
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de linhas: ");
+            int linhas = Transforma_Para_Int(Console.ReadLine());
+            Console.Write("Digite a quantidade de colunas: ");
+            int colunas = Transforma_Para_Int(Console.ReadLine());
+
+            int[,] mat = new int[linhas, colunas];
+
+            for (int i = 0; i < linhas; i++)
+            {
+                string[] values = Console.ReadLine().Split(" ");
+
+                for (int j = 0; j < colunas; j++)
+                {
+                    int numero = Transforma_Para_Int(values[j]);
+                    mat[i, j] = numero;
+                }
+            }
+            int number = Transforma_Para_Int(Console.ReadLine());
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    if (number == mat[i, j])
+                    {
+                        Console.WriteLine($"Position {i},{j}");
+                        if (i > 0)
+                        {
+                            Console.WriteLine($"Up: {mat[i - 1, j]}");
+                        }
+                        if (j > 0)
+                        {
+                            Console.WriteLine($"Left: {mat[i, j - 1]}");
+                        }
+                        if (i < linhas - 1)
+                        {
+                            Console.WriteLine($"Down: {mat[i + 1, j]}");
+                        }
+                        if (j < colunas - 1)
+                        {
+                            Console.WriteLine($"Right: {mat[i, j + 1]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Exercicios/Exercicio9/Exercicio9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exercicio9
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo invC = CultureInfo.InvariantCulture;
+
+            Console.Write("How many employees will be registered? ");
+            int number = int.Parse(Console.ReadLine());
+
+            List<Employee> listOfEmployee = new List<Employee>();
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), invC);
+                listOfEmployee.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+            Console.Write("Enter the employee id that will have salary increased: ");
+            int idSalary = int.Parse(Console.ReadLine());
+
+            Employee employ = listOfEmployee.Find(x => x.Id == idSalary);
+            if (employ == null)
+            {
+                Console.WriteLine("This id does not exist!\n");
+            }
+            else
+            {
+                Console.Write("Enter the percentage: ");
+                float percentageSalary = float.Parse(Console.ReadLine(), invC);
+                employ.IncreaseSalary(percentageSalary);
+            }
+            Console.WriteLine("Updated list of employees:");
+            foreach (var item in listOfEmployee)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
