8c7380085ae55fb9e563229fbadc3eb8a03ad9ad
AndrewMoreiraN <andrewmoreira02@hotmail.com>
2022-05-10 19:54:07 -0300

    Other entities and second migration

--- Projetos/SalesWebMvc/SalesWebMvc/Controllers/DepartmentsController.cs
+++ Projetos/SalesWebMvc/SalesWebMvc/Controllers/DepartmentsController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
@@ -50,7 +49,7 @@ namespace SalesWebMvc.Controllers
         }
 
         // POST: Departments/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -82,7 +81,7 @@ namespace SalesWebMvc.Controllers
         }
 
         // POST: Departments/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
--- Projetos/SalesWebMvc/SalesWebMvc/Models/Department.cs
+++ Projetos/SalesWebMvc/SalesWebMvc/Models/Department.cs
@@ -1,8 +1,40 @@
-﻿namespace SalesWebMvc.Models
+﻿using SalesWebMvc.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public void RemoveSeller(Seller seller)
+        {
+            Sellers.Remove(seller);
+        }
+
+        public double TotalSales(DateTime intial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(intial, final));
+        }
     }
 }
